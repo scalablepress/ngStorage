6e27b9d83304054694e12c613bc06278ed9e859c
ericz <really.ez@gmail.com>
2015-03-25 11:15:04 -0700

    Add null check on e.key for storage to handle .clear calls

--- ngStorage.js
+++ ngStorage.js
@@ -93,6 +93,10 @@
 
                 // #6: Use `$window.addEventListener` instead of `angular.element` to avoid the jQuery-specific `event.originalEvent`
                 'localStorage' === storageType && $window.addEventListener && $window.addEventListener('storage', function(event) {
+                    if (event.key == null) {
+                      // DOM storage event will send a null key on storage.clear() called
+                      return;
+                    }
                     if ('ngStorage-' === event.key.slice(0, 10)) {
                         event.newValue ? $storage[event.key.slice(10)] = angular.fromJson(event.newValue) : delete $storage[event.key.slice(10)];
 
